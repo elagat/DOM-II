5dbccc0d8eadaa294de35d84bca545cfb4b2a43d
Emily Lagattolla <elagattolla@gmail.com>
2019-06-18 17:45:53 -0500

    added fifth event listener on doubleclick

--- js/index.js
+++ js/index.js
@@ -7,18 +7,36 @@ navColor.addEventListener('mouseover', event => {
 
 navColor.addEventListener('mouseout', event => {
   event.target.style.color = 'pink';
-})
+});
 
-// Change banner image on click (3)
+// Change banner image on click & doubleclick (3, 4)
 
 let bannerImage = document.querySelector('img');
 bannerImage.addEventListener('click', event => {
-  event.target.style.border = '2px solid dodgerblue';
+  event.target.style.border = '3px solid dodgerblue';
+});
+
+bannerImage.addEventListener('dblclick', event => {
+  event.target.style.border = '3px solid pink';
 })
 
-// Change h1 title on drag (4)
+// Change ... on scroll (4)
+
+// window.addEventListener('scroll', event => {
+//   document.querySelector('home intro img').style.opacity = '0.2';
+//
+// });
+
+// Change h1 title on drag (5)
 
 let title = document.querySelector('h1');
 title.addEventListener('drag', event => {
   event.target.textContent = 'Funny Bus';
-})
+});
+
+// Change h2 titles on keydown (6)
+
+// let titleSmall = document.querySelectorAll('h2');
+// titleSmall.addEventListener('keydown', event => {
+//   event.target.style.color = 'dodgerblue';
+// })
